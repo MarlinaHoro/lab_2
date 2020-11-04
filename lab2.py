8c9c06f1d329d8a943e6dfe0f4f5a3f8dfa2e6c4
marlina <marlina1@gmail.com>
2020-11-04 13:58:19 +0700

    file Python

---
+++ lab2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=eval(input("masukan nilai a: "))
+b=eval(input("masukkan nilai b: "))
+print("Variable a=" ,a)
+print("Variable b=" ,b)
+print("hasil penggabungan {1}&{0}=%d" .format(a,b) %(a+b))
+
+#Konversi nilai vaeriable
+a=int(a)
+b=int(b)
+print("hasil penjuamlahan {1}+{0}=%d" .format (a,b) %(a+b)) 
+print("hasil pembagian {1}/{0}=%d" .format(a,b) %(a/b)) 
+    
